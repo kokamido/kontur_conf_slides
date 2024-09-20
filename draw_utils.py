1b5061315444d20f96ad7c12eb41f0c235dc3471
alexandr.pankratov <alexandr.pankratov@kontur.ru>
2024-09-20 14:31:09 +0500

    Настроил Lab Grotesque для графиков

--- draw_utils.py
+++ draw_utils.py
@@ -1,10 +1,23 @@
 from graphviz import Digraph
 from micrograd.micrograd.engine import Value
 import matplotlib.pyplot as plt
+from matplotlib import font_manager
 
 
 COLOR_PINK = "#ff007f"
 COLOR_VIOLET = "#9700ff"
+
+
+font_path = 'etc/LabGrotesqueK-Regular.ttf'  # Replace with your font file path
+
+font_manager.fontManager.addfont(font_path)
+font_prop = font_manager.FontProperties(fname=font_path, size=24)
+
+plt.rcParams['font.size'] = 24
+plt.rcParams['font.family'] = font_prop.get_name()
+
+print('Глобальные настройки шрифтов посеттил')
+# Ну да, грязь, а что ты мне сделаешь?
 
 
 def trace(root):
@@ -29,20 +42,36 @@ def draw_dot(root, show_grads=False, format="svg", rankdir="LR"):
     assert rankdir in ["LR", "TB"]
     nodes, edges = trace(root)
     dot = Digraph(
-        format=format, graph_attr={"rankdir": rankdir}
+        format=format, 
+        graph_attr={"rankdir": rankdir, "bgcolor": "transparent", "fontname":"Lab Grotesque K"},
+        node_attr={"fontname":"Lab Grotesque K", "fontsize":'16'},
+        edge_attr={"fontname":"Lab Grotesque K", "fontsize":'16'}
     )  # , node_attr={'rankdir': 'TB'})
 
     for n in nodes:
-        label = f"name {n.label} | data {n.data:.4}"
+        label = f"{n.label} | data {n.data:.4}"
         if show_grads:
             label = label + f" | grad {n.grad:.4}"
-        dot.node(name=str(id(n)), label=label, shape="record")
+        dot.node(
+            name=str(id(n)),
+            label=label,
+            shape="record",
+            fontcolor=COLOR_PINK,
+            color=COLOR_VIOLET,
+            penwidth="3.0",
+        )
         if n._op:
-            dot.node(name=str(id(n)) + n._op, label=n._op)
-            dot.edge(str(id(n)) + n._op, str(id(n)))
+            dot.node(
+                name=str(id(n)) + n._op,
+                label=n._op,
+                fontcolor=COLOR_PINK,
+                color=COLOR_VIOLET,
+                penwidth="3.0",
+            )
+            dot.edge(str(id(n)) + n._op, str(id(n)), color=COLOR_VIOLET, penwidth="3.0")
 
     for n1, n2 in edges:
-        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op, color=COLOR_VIOLET, penwidth="3.0")
 
     return dot
 
