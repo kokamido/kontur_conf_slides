b75f44e262e37bef43f9889dd750253f138e62e1
alexandr.pankratov <alexandr.pankratov@kontur.ru>
2024-09-24 10:25:25 +0500

    Выхлоп профилировщика и презентация

--- run_everything.sh
+++ run_everything.sh
@@ -3,12 +3,19 @@ rm -rf etc/profiler_outputs/*
 echo running 'python scripts/cpu_profile.py'
 python scripts/cpu_profile.py
 
-total_percents=$(cat etc/profiler_outputs/cpu_forward_profiling | awk '{print $2}' | grep '\.' | tr -d % | awk '{s+=$1} END {print s}')
+total_percents_self_cpu=$(cat etc/profiler_outputs/cpu_forward_profiling | awk '{print $2}' | grep '\.' | tr -d % | awk '{s+=$1} END {print s}')
 
-echo "Sum of self_cpu% col in etc/profiler_outputs/cpu_forward_profiling is $total_percents" 
+echo "[LOG] Sum of self_cpu% col in etc/profiler_outputs/cpu_forward_profiling is $total_percents_self_cpu" 
 
 echo running 'python scripts/gpu_profile.py'
 python scripts/gpu_profile.py
+
+total_percents_self_cuda=$(cat etc/profiler_outputs/gpu_forward_profiling | sed -E 's/[ ]{2,}/\t/g' |cut -f9 | grep -oP "\d+\.\d+%" | tr -d %| awk '{s+=$1} END {print s}')
+echo "[LOG] Sum of self_cuda% col in etc/profiler_outputs/gpu_forward_profiling is $total_percents_self_cuda" 
+
+total_percents_self_cuda_no_aten=$(cat etc/profiler_outputs/gpu_forward_profiling | grep -v 'aten::' | sed -E 's/[ ]{2,}/\t/g' |cut -f9 | grep -oP "\d+\.\d+%" | tr -d %| awk '{s+=$1} END {print s}')
+echo "[LOG] Sum of self_cuda% without aten:: col in etc/profiler_outputs/gpu_forward_profiling is $total_percents_self_cuda_no_aten" 
+
 
 echo running nsys profile
 nsys profile \
